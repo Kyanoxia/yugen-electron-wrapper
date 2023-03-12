4c4dead07cd85c01827a20fed960548b056569ed
SpektacleFR <zacharygough07@gmail.com>
2023-03-11 19:04:05 -0500

    Yugen Arc Dark Implementation

--- build/preload.js
+++ build/preload.js
@@ -1,5 +1,5 @@
 "use strict";
-/*window.addEventListener('DOMContentLoaded', function () {
+window.addEventListener('DOMContentLoaded', function () {
     // adding css
     const head = document.getElementsByTagName('head')[0];
     const link = document.createElement('link');
@@ -8,14 +8,5 @@
     link.type = 'text/css';
     link.href = 'yugen://' + process.env['APP_PATH'] + '/assets/styles.css';
     link.media = 'all';
-
     head.appendChild(link);
-});*/
-const head = document.getElementsByTagName('head')[0];
-const link = document.createElement('link');
-link.id = 'custom-theme';
-link.rel = 'stylesheet';
-link.type = 'text/css';
-link.href = 'yugen://' + process.env['APP_PATH'] + '/assets/styles.css';
-link.media = 'all';
-head.appendChild(link);
+});
